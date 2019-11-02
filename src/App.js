e503f4d2704c170aa4bac0739ca08e228b8917c7
rupson <44114899+rupson@users.noreply.github.com>
2019-11-02 21:18:30 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useQuery } from "react-apollo";
+import gql from "graphql-tag";
+
+const GET_POKEMON_INFO = gql`
+    {
+        pokemons(first: 150) {
+            id
+            number
+            name
+            image
+            evolutions {
+                id
+                number
+                name
+                image
+            }
+        }
+    }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { data, loading, error } = useQuery(GET_POKEMON_INFO);
+
+    return (
+        <>
+            {loading && <div>Loading...</div>}
+            {error && <div>Error!</div>}
+            {!loading && !error && (
+                <>
+                    <div>
+                        {data &&
+                            data.pokemons &&
+                            data.pokemons.map((pokemon, index) => (
+                                <div key={index}>
+                                    <img
+                                        src={pokemon.image}
+                                        alt={pokemon.name}
+                                    />
+                                    <div>
+                                        <span>{pokemon.name}</span>
+                                        {pokemon.evolutions &&
+                                            pokemon.evolutions.length !== 0 && (
+                                                <>
+                                                    <p>Evolutions:</p>
+                                                    {pokemon.evolutions.map(
+                                                        (evolution, index) => (
+                                                            <p key={index}>
+                                                                {evolution.name}
+                                                            </p>
+                                                        ),
+                                                    )}
+                                                </>
+                                            )}
+                                    </div>
+                                </div>
+                            ))}
+                    </div>
+                </>
+            )}
+        </>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache();
+const link = new HttpLink({ uri: `https://graphql-pokemon.now.sh/` });
+
+const client = new ApolloClient({ cache, link });
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById("root"),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
