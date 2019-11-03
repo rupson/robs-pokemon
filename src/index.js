16cc7fccb9060d53edd5a27a027da4406eabe731
rupson <44114899+rupson@users.noreply.github.com>
2019-11-03 20:21:35 +0000

    added note for where the tutorial came from

--- src/index.js
+++ src/index.js
@@ -24,3 +24,6 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// made using tutorial from:
+// https://www.sitepoint.com/how-to-build-a-web-app-with-graphql-and-react/
